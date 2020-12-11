52eabf9b3919d903d688f3c7c2175bbb056cd2c2
akimi-funakosi <akimifunakoshi@gmail.com>
2020-12-11 21:05:29 +0900

    ログイン機能実装

--- todo_list/forms.py
+++ todo_list/forms.py
@@ -1,8 +1,31 @@
 from django import forms
 from .models import List
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ListForm(forms.ModelForm):
     class Meta:
         model = List
         fields = ["item", "completed"]
+
+
+class UserCreateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #htmlの表示を変更可能にします
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+    class Meta:
+       model = User
+       fields = ("username", "password1", "password2",)
+
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       #htmlの表示を変更可能にします
+       self.fields['username'].widget.attrs['class'] = 'form-control'
+       self.fields['password'].widget.attrs['class'] = 'form-control'
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import List
-from .forms import ListForm
+from .forms import ListForm, LoginForm
 from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.views.generic import CreateView
+from . forms import UserCreateForm
+from django.views.generic import View
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
 
     if request.method == 'POST':
@@ -12,38 +20,38 @@ def home(request):
             form.save()
             all_items = List.objects.all
             messages.success(request, ('Item Has Been Added To List'))
-            return render(request, 'home.html', {'all_items': all_items})
+            return render(request, 'list.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
-        return render(request, 'home.html', {'all_items': all_items})
+        return render(request, 'list.html', {'all_items': all_items})
 
-
+@login_required
 def about(request):
     context = {'first_name': 'Harry', 'last_name': 'Potter'}
     return render(request, 'about.html', context)
 
-
+@login_required
 def delete(request, list_id):
     item = List.objects.get(pk=list_id)
     item.delete()
     messages.success(request, ('Item Has Been Deleted from List!'))
     return redirect('home')
 
-
+@login_required
 def uncomplete(request, list_id):
     item = List.objects.get(pk=list_id)
     item.completed = False
     item.save()
     return redirect('home')
 
-
+@login_required
 def complete(request, list_id):
     item = List.objects.get(pk=list_id)
     item.completed = True
     item.save()
     return redirect('home')
 
-
+@login_required
 def edit(request, list_id):
     if request.method == 'POST':
         item = List.objects.get(pk=list_id)
@@ -57,3 +65,58 @@ def edit(request, list_id):
     else:
         item = List.objects.get(pk=list_id)
         return render(request, 'edit.html', {'item': item})
+
+
+class Create_account(CreateView):
+    def get(self, request, *args, **kwargs):
+        #ここ
+        form = UserCreateForm(request.POST)
+        return  render(request, 'create_account.html', {'form': form,})
+        
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            #フォームから'username'を読み取る
+            username = form.cleaned_data.get('username')
+            #フォームから'password1'を読み取る
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'create_account.html', {'form': form,})
+create_account = Create_account.as_view()
+
+class Account_login(View):
+    def post(self, request, *arg, **kwargs):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'login.html', {'form': form,})
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        return render(request, 'login.html', {'form': form,})
+
+account_login = Account_login.as_view()
+
+@login_required
+def profile(request):
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item Has Been Added To List'))
+            return render(request, 'list.html', {'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, 'list.html', {'all_items': all_items})
+
+
+    return render(request, 'list.html')
